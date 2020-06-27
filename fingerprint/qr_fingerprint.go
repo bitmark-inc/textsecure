33da33d6504ecc9f46a990b381bc745cee5aa48d
Aaron <aaron@convive.io>
2020-06-27 19:05:46 +0200

    initial test

--- fingerprint/qr_fingerprint.go
+++ fingerprint/qr_fingerprint.go
@@ -5,21 +5,22 @@ import (
 	"github.com/signal-golang/textsecure/axolotl/protobuf"
 	"golang.org/x/text/encoding/charmap"
 )
+
 //based on https://github.com/signalapp/libsignal-protocol-java/blob/3662b6d705ae4162ad8b3a242daf35171edbb068/java/src/main/java/org/whispersystems/libsignal/fingerprint/ScannableFingerprint.java
 //and https://github.com/signalapp/Signal-Android/blob/6f39f9849a002f6361d192a00fbd7c52ffaf3bba/app/src/main/java/org/thoughtcrime/securesms/VerifyIdentityActivity.java#
-func CreateQRFingerprint(version uint32, localFingerprint []byte, remoteFingerprint []byte) (string, error) {
+func CreateQRFingerprint(version uint32, localFingerprint []byte, remoteFingerprint []byte) ([]byte, error) {
 	data, err := getMarshalledCombinedFingerprints(version, localFingerprint, remoteFingerprint)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-    //https://github.com/signalapp/Signal-Android/blob/6f39f9849a002f6361d192a00fbd7c52ffaf3bba/app/src/main/java/org/thoughtcrime/securesms/VerifyIdentityActivity.java#L501
+	//https://github.com/signalapp/Signal-Android/blob/6f39f9849a002f6361d192a00fbd7c52ffaf3bba/app/src/main/java/org/thoughtcrime/securesms/VerifyIdentityActivity.java#L501
 	decoder := charmap.ISO8859_1.NewDecoder()
 	var qrCodeContent []byte
 	qrCodeContent, err = decoder.Bytes(data)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(qrCodeContent), nil
+	return qrCodeContent, nil
 }
 
 func getMarshalledCombinedFingerprints(version uint32, localFingerprint []byte, remoteFingerprint []byte) ([]byte, error) {
@@ -32,7 +33,7 @@ func getMarshalledCombinedFingerprints(version uint32, localFingerprint []byte, 
 			Content: remoteFingerprint[:32],
 		},
 	}
-    //https://github.com/signalapp/libsignal-protocol-java/blob/3662b6d705ae4162ad8b3a242daf35171edbb068/java/src/main/java/org/whispersystems/libsignal/fingerprint/ScannableFingerprint.java#L43
+	//https://github.com/signalapp/libsignal-protocol-java/blob/3662b6d705ae4162ad8b3a242daf35171edbb068/java/src/main/java/org/whispersystems/libsignal/fingerprint/ScannableFingerprint.java#L43
 	data, err := proto.Marshal(combinedFingerprints)
 	if err != nil {
 		return nil, err
@@ -42,8 +43,8 @@ func getMarshalledCombinedFingerprints(version uint32, localFingerprint []byte, 
 
 //based on https://github.com/signalapp/Signal-Android/blob/6f39f9849a002f6361d192a00fbd7c52ffaf3bba/app/src/main/java/org/thoughtcrime/securesms/VerifyIdentityActivity.java#L403
 //and https://github.com/signalapp/libsignal-protocol-java/blob/3662b6d705ae4162ad8b3a242daf35171edbb068/java/src/main/java/org/whispersystems/libsignal/fingerprint/ScannableFingerprint.java#L54
-func ScanQRFingerprint(qrCodeContent string) (*textsecure.CombinedFingerprints, error) {
-	content := []byte(qrCodeContent)
+func ScanQRFingerprint(qrCodeContent []byte) (*textsecure.CombinedFingerprints, error) {
+	content := qrCodeContent
 	encoder := charmap.ISO8859_1.NewEncoder()
 	data, err := encoder.Bytes(content)
 	if err != nil {
